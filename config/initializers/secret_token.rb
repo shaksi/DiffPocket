af31523a81873a9edbde2d305050dd6298dc8a82
Shidane <sshidane@UK-sshidaneA01.(none)>
2012-06-10 01:00:41 +0200

    initial commit of rails foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diffpocket::Application.config.secret_token = '7dfd86cbb6120513d8b0971bc83eea30ebc5d4c10907203ed3f4e1c04a3a81ca65f1d992715ef6c291808d997e6ba67a644f3672c837c69b2d02ad1adcc22b9e'
