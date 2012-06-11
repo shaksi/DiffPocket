3f627b14a47aef2111282d13610f07892fa20a89
Shidane <sshidane@UK-sshidaneA01.(none)>
2012-06-11 15:13:56 +0200

    getting users and sessions working. (can register, login, logout)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
+  def authorise
+    redirect_to login_url, alert: "Not Authorised" if current_user.nil?
+    
+  end
 end
